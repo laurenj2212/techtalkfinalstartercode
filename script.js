0d57e7f1186d6b00bfe9fa13cbaa9b3ca2a911fd
Lauren Joseph <ljoseph.student@hsfi.us>
2017-07-07 19:02:09 +0000

    my comment

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
-
+    console.log( "ready!" );
+    $('#hero').click(function() {
+console.log('ok')
 
 });
+});
+
+
+
+console.log('wow')
